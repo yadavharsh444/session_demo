ac6c6198f847502e2764b68b820b5ab9ffa28e14
Harsh yadav <harsh@Harshs-MacBook-Air.local>
2023-06-10 09:24:06 +0530

    Dev test

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express')
 
 const app = express();
 
-////
 
 app.get("/",(req,res)=>{
     res.send("Welcome to the code")
 })
-
+//
 
 app.listen(3000, ()=>{
     console.log("Connected to the server")
